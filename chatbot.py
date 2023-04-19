dafd03d543e368c2983802e96908167327abb471
AkshVert <123250727+AkshVert@users.noreply.github.com>
2023-04-19 22:18:12 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,15 +4,15 @@ from nltk.chat.util import Chat, reflections
 pairs = [    
     ["(hi|hello|hey)", ["Hello, how was your day?"]],
     ["(good|ok|well|not good|bad|awesome|nice|fine)",["I wish to have you a great day ahead!"]],
-    ["thanks|thank you|ok",["******Talking to you feels good!******\nNow can i tell you your todays'task?"]],
-    ["no", ["Ok, do u want thought of the day(y or n)"]],
-    ["ready",["Have a splendid day and energy for doing the tasks, Bye"]],
+    ["thanks|thank you|ok",["******Talking to you feels good!******"]],
+    ["cool", ["Ok, do u want thought of the day(y or n)"]],
     ["what is your name?", ["My name is Maniac, How can I help you?"]],
     ["how are you?", ["I'm doing well, thanks for asking."]],
     ["what is your age?", ["I'm of your age only."]],
     ["what is your (location|city)?", ['I exist on your screen.']],
     ["How you were (created|made)?", ["I was created by A under idea of M."]],
     ["(y|Thought of the day)",["Love the life"]],
+    ["n",["Ok"]]
 ]
 
 chatbot = Chat(pairs, reflections)
